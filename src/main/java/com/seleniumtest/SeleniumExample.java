1e6f3d169a2835e7ae9866e3518e4e3e154eec5f
aleksahadzic <aki.hadza@gmail.com>
2023-11-14 13:38:24 +0100

    adding additional exception and refactoring code.

--- src/main/java/com/seleniumtest/SeleniumExample.java
+++ src/main/java/com/seleniumtest/SeleniumExample.java
@@ -1,4 +1,7 @@
 package com.seleniumtest;
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
 import javax.print.attribute.standard.MediaSize.NA;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -7,11 +10,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.v113.indexeddb.model.Key;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SeleniumExample {
 
     public static WebDriver driver;
     public static Actions actions; 
+    public static WebDriverWait wait;
+
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:/Windows/chromedriver.exe");
 
@@ -19,10 +27,12 @@ public class SeleniumExample {
 
         driver.get("https://dell:8443/now/MainDesktop.jsp");
 
+        wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+
         Thread.sleep(1000);
         logMeIn("system","training");
         Thread.sleep(1000);
-        addMethodToProcessDefinition("BookTransaction","HeaderRowProcess.abs","com/bookshop","BookTransactionSession.xml");
+        addMethodToProcessDefinition("BookTransaction","HeaderRowProcess.abs","com/bookshop");
     }
 
     public static void logMeIn(String username,String password) throws InterruptedException{
@@ -32,63 +42,61 @@ public class SeleniumExample {
        }
        catch(Exception e){
         //not secure website exception
-          Thread.sleep(1000);
-          WebElement notSecure = driver.findElement(By.xpath("/html/body/div/div[2]/button[3]"));
+          WebElement notSecure = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div[2]/button[3]")));
           notSecure.click();
           WebElement proceed = driver.findElement(By.xpath("/html/body/div/div[3]/p[2]/a"));
           proceed.click();
         }
-        Thread.sleep(1000);
-        WebElement usernameField = driver.findElement(By.id("j_username-inputEl"));
-        usernameField.click();
-        usernameField.sendKeys(username);
-        Thread.sleep(1000);
-        WebElement passwordField = driver.findElement(By.id("j_password-inputEl"));
-        passwordField.click();
-        passwordField.sendKeys(password);
+        WebElement usernameField = wait.until(ExpectedConditions.elementToBeClickable(By.id("j_username-inputEl")));
+        clickAndSendKeys(usernameField, username);
+        WebElement passwordField = wait.until(ExpectedConditions.elementToBeClickable(By.id("j_password-inputEl")));
+        clickAndSendKeys(passwordField, password);
         actions = new Actions(driver);
         actions.sendKeys(Keys.RETURN).perform();
     }
-    public static void addMethodToProcessDefinition(String processname,String processtype,String Path,String Name) throws InterruptedException{
-        WebElement body = driver.findElement(By.tagName("body"));
-       // actions = new Actions(driver);
+    public static void clickAndSendKeys(WebElement element, String key ){
+        element.click();
+        element.sendKeys(key);
+    }
+    public static void addMethodToProcessDefinition(String processname,String processtype,String processPath) throws InterruptedException{
+       String processName = processname + "Session.xml";
+
+       WebElement body = driver.findElement(By.tagName("body"));
        body.sendKeys(Keys.ALT + "M" + Keys.BACK_SPACE);
-       Thread.sleep(1000);
-       WebElement search = driver.findElement(By.id("search-menu-inputEl")); 
-       search.click();
-       search.sendKeys("Process Definition");
+
+       
+       WebElement search = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search-menu-inputEl"))); 
+       clickAndSendKeys(search, "Process Definition"); 
        search.sendKeys(Keys.ENTER);
-       Thread.sleep(1000);
-       WebElement textInput = driver.findElement(By.name("textfield-2491-inputEl"));
-       textInput.click();
-       textInput.sendKeys(Keys.chord(Keys.ALT,Keys.F2));
-       Thread.sleep(1000);
-       WebElement code = driver.findElement(By.name("Code"));
-       WebElement description = driver.findElement(By.name("Description"));
-       WebElement menuDescription = driver.findElement(By.name("MenuDescr"));
-       WebElement alternateText = driver.findElement(By.name("AlternateText"));
-       code.click();
-       code.sendKeys(processname);
-       description.click();
-       description.sendKeys(processname);
-       menuDescription.click();
-       menuDescription.sendKeys(processname);
-       alternateText.click();
-       alternateText.sendKeys(processname);
+
+       
+       WebElement textInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("textfield-2491-inputEl")));
+       clickAndSendKeys(textInput, Keys.chord(Keys.ALT, Keys.F2));
+       
+       WebElement code = wait.until(ExpectedConditions.elementToBeClickable(By.name("Code")));
+       WebElement description = wait.until(ExpectedConditions.elementToBeClickable(By.name("Description")));
+       WebElement menuDescription = wait.until(ExpectedConditions.elementToBeClickable(By.name("MenuDescr")));
+       WebElement alternateText = wait.until(ExpectedConditions.elementToBeClickable(By.name("AlternateText")));
+       clickAndSendKeys(code, processname);
+       clickAndSendKeys(description, processname);
+       clickAndSendKeys(menuDescription, processname);
+       clickAndSendKeys(alternateText, processname);
        WebElement element = driver.findElement(By.id("tab-2600"));
        element.click();
        WebElement urlProcess = driver.findElement(By.id("combo-2529-inputEl"));
-       urlProcess.click();
-       urlProcess.sendKeys(processtype);
-       WebElement path = driver.findElement(By.id("textfield-2530-inputEl"));
-       path.click();
-       path.sendKeys(Path);
-       WebElement name = driver.findElement(By.id("textfield-2531-inputEl"));
-       name.click(); 
-       name.sendKeys(Name);
-       name.sendKeys(Keys.chord(Keys.ALT,Keys.F3));
-       System.out.println("Process saved sucessfully "+ Name);
+       clickAndSendKeys(urlProcess, processtype);
 
+       WebElement path = driver.findElement(By.id("textfield-2530-inputEl"));
+       clickAndSendKeys(path,processPath);
+       WebElement name = driver.findElement(By.id("textfield-2531-inputEl"));
+       clickAndSendKeys(name, processName);
+       name.sendKeys(Keys.chord(Keys.ALT,Keys.F3));
+       try{
+       WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div/div[1]/div[2]/div[4]/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div[1]/table/tbody/tr[1]/td[2]/div")));
+       System.out.println("Could not create" + processName + ".Reason: " + errorMessage.getText());
+       }catch(org.openqa.selenium.NoSuchElementException e){
+            System.out.println(processName + " was created sucessfully.");
+       }
     }
     
 
